d292d1afd74e61e3b3cf4a5aade5bcb9a2149149
arhipovalana <lana.qa.2019@gmail.com>
2020-06-28 21:14:41 +0300

    Java collections basics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,67 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
 
-        ProjectFunctions functions = new ProjectFunctions();
+//        ProjectFunctions functions = new ProjectFunctions();
+//
+//        String text = functions.getWelcomeText("Valera");
+//        System.out.println(text);
 
-        String text = functions.getWelcomeText("Valera");
-        System.out.println(text);
+        String text = "Lorem Ipsum - is simply dummy text of the printing, and typesetting industry!";
+
+        String[] words = text.split(" ");
+        System.out.println("Word count is " + words.length);
+//        System.out.println(words[4]);
+        for (int i = 0; i < words.length; i = i + 2) {
+            System.out.println("The word with id " + i + " is " + words[i]);
+        }
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+//        spiski
+
+        List<String> names = new ArrayList<> ();
+        names.add("Alexandra");
+        names.add("Juel");
+        names.add("Max");
+        names.add("Upss Upss");
+
+        System.out.println(names.get(1));
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+//        hashmap
+        HashMap<String, Integer> bookUniqueWords = new HashMap<>();
+        bookUniqueWords.put("the", 1);
+        bookUniqueWords.put("student", 1);
+        bookUniqueWords.put("is", 1);
+        bookUniqueWords.put("the", 2);
+
+        for (Map.Entry<String, Integer> entry: bookUniqueWords.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue()); // getvalue - vitashit value
+        }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
